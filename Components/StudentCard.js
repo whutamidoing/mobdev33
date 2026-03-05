11c373b7372905fa0bee3092e6fd4e29cfd8af34
denshooo <denisebocadoschl@gmail.com>
2026-03-05 15:29:17 +0800

    fixed formatting

--- Components/StudentCard.js
+++ Components/StudentCard.js
@@ -3,11 +3,13 @@ import { Checkbox } from 'expo-checkbox';
 
 function StudentCard(props){
     return(
-    <View style={{borderColor:'black',borderWidth:1,width:'80%',padding:5,marginBottom:10}}>
-        <Text>ID:{props.id}</Text>
-        <Text>Name:{props.name}</Text>
-        <Text>Course:{props.course}</Text>
-        <Text>Year:{props.year}</Text>
+    <View style={{display:'flex', flexDirection:'row', justifyContent:'space-between', alignItems:'center', borderColor:'black',borderWidth:1,width:'80%',padding:5,marginBottom:10}}>
+        <View style={{}}>
+            <Text>ID:{props.id}</Text>
+            <Text>Name:{props.name}</Text>
+            <Text>Course:{props.course}</Text>
+            <Text>Year:{props.year}</Text>
+        </View>
         <Checkbox value={props.isEnrolled}
         onValueChange={(val)=>props.changeEnrolled(val,props.id)}/>
     </View>
