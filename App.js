3c48d2378cc59a283fcae3f88106ee2c50dd7bb1
whutamidoing <negispringfield23@gmail.com>
2026-03-10 13:17:38 +0800

    I did it my way

--- App.js
+++ App.js
@@ -1,44 +1,38 @@
-
-import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
-import StudentCard,{CourseCard} from './Components/StudentCard'
-import {useState} from 'react'
-import StudentForm from './Components/StudentForm';
-
-
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
+import StudentCard, { CourseCard } from "./Components/StudentCard";
+import { useState } from "react";
+import StudentForm from "./Components/StudentForm";
 
 export default function App() {
+  const [studentList, setStudentList] = useState([
+    { id: 1, name: "Patrick", course: "CpE", year: "4th Year", enrolled: true },
+    { id: 2, name: "Chiz", course: "IT", year: "3rd Year", enrolled: false },
+    { id: 3, name: "Gran", course: "CS", year: "2nd Year", enrolled: true },
+    { id: 4, name: "Marr", course: "ICT", year: "3rd Year", enrolled: false },
+  ]);
 
-  const [studentList,setStudentList]=useState([
-    {id:1, name:"Patrick",course:"CpE",year:"4th Year", enrolled:true},
-    {id:2, name:"Chiz",course:"IT",year:"3rd Year", enrolled:false},
-    {id:3, name:"Gran",course:"CS",year:"2nd Year", enrolled:true},
-    {id:4, name:"Marr",course:"ICT",year:"3rd Year", enrolled:false}
-  ])
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [course, setCourse] = useState("");
+  const [year, setYear] = useState("");
 
-  const [id,setId]=useState('')
-  const [name,setName]=useState('')
-  const [course,setCourse]=useState('')
-  const [year,setYear]=useState('')
+  const addStudent = () => {
+    const toAdd = { id: id, name: name, course: course, year: year };
+    console.log(studentList.find((student) => student.id == toAdd.id) == null);
+    if (studentList.find((student) => student.id == toAdd.id) != null) {
+      Alert.alert("Student already exist.");
+      return;
+    }
+    setStudentList([...studentList, toAdd]);
+    Alert.alert("Student Added", `Name:${name} Course:${course} Year:${year}`);
+  };
 
-  const addStudent=()=>{
-    let i=0;
-   for(;i<studentList.length && studentList[i].id != id;i++){}
-  if(i>=studentList.length){
-      const toAdd = {id:id,name:name,course:course,year:year}
-      setStudentList([...studentList,toAdd])
-      
-      Alert.alert("Student Added", `Name:${name} Course:${course} Year:${year}`)
-  }else{
-    Alert.alert("ID already exists", "Please enter a unique ID")
-  }
-}
-
-  const changeEnrolledStatus=(val,studentId)=>{
-    setStudentList(previousList=>
-      previousList.map((student)=>
-        student.id===studentId ? {...student, enrolled:val} : student
-      )
-    )
+  const changeEnrolledStatus = (val, studentId) => {
+    setStudentList((previousList) =>
+      previousList.map((student) =>
+        student.id === studentId ? { ...student, enrolled: val } : student,
+      ),
+    );
 
     // const newlist = [...studentList];
 
@@ -46,36 +40,43 @@ export default function App() {
     //   if(newlist[i].id == studentId){
     //     newlist[i].enrolled = val}
     //   }
-    
-    // setStudentList(newlist)
-    console.log("Test", studentId, val)
-  }
 
+    // setStudentList(newlist)
+    console.log("Test", studentId, val);
+  };
 
   return (
-
     <View style={styles.container}>
+      {studentList.map((student, index) => {
+        return (
+          <StudentCard
+            key={index}
+            id={student.id}
+            changeEnrolled={changeEnrolledStatus}
+            name={student.name}
+            course={student.course}
+            year={student.year}
+            isEnrolled={student.enrolled}
+          />
+        );
+      })}
 
-  
-    {studentList.map((student,index)=>{
-      return(
-        <StudentCard key={index} id={student.id} changeEnrolled={changeEnrolledStatus} name={student.name} course={student.course} year={student.year} isEnrolled={student.enrolled}/>
-      )
-      
-  })}
-
-   <StudentForm onChangeID={setId} onChangeName={setName} onChangeCourse={setCourse} onChangeYear={setYear} addStudent={addStudent} />
+      <StudentForm
+        onChangeID={setId}
+        onChangeName={setName}
+        onChangeCourse={setCourse}
+        onChangeYear={setYear}
+        addStudent={addStudent}
+      />
     </View>
- 
-   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
